b93afc4af732f76c4c3e1ae6a613fbbfaaac1eed
HasutoSasaki <hasuto.sa@gmail.com>
2025-06-14 08:32:06 +0900

    feat: add multiple examples for functions, variable declarations, and results handling

---
+++ 01-basics/functions/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func add(x int, y int)int{
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(1, 2))
+	fmt.Println(add(3, 4))
+	fmt.Println(add(5, 6))
+	fmt.Println(add(7, 8))
+	fmt.Println(add(9, 10))
+	fmt.Println(add(11, 12))
+	fmt.Println(add(13, 14))
+	fmt.Println(add(15, 16))
+	fmt.Println(add(17, 18))
+	fmt.Println(add(19, 20))
+}
